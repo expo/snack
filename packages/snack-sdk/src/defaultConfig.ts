5c3b61ba7af933a5f0367b1b5b75b2970684fb57
Hein Rutjes <hrutjes@gmail.com>
2020-12-09 19:46:23 +0100

    [sdk] Set default version to 40.0.0

--- packages/snack-sdk/src/defaultConfig.ts
+++ packages/snack-sdk/src/defaultConfig.ts
@@ -4,7 +4,7 @@ export const apiURL: string = 'https://exp.host';
 export const snackagerURL: string = 'https://snackager.expo.io';
 export const host: string = 'expo.io';
 
-export const sdkVersion: SDKVersion = '39.0.0';
+export const sdkVersion: SDKVersion = '40.0.0';
 
 export const SnackIdentityState: SnackState = {
   sdkVersion,
