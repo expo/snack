9730eeb4fbf821b337346edcce02a4b7ad4e6ac1
Hein Rutjes <hrutjes@gmail.com>
2021-02-04 12:45:51 +0100

    [tools] Faster dev workflow using ts-node-dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,36 @@
 const path = require('path');
 
-const interpreter = path.resolve(__dirname, 'node_modules/.bin/ts-node');
+const interpreter = path.resolve(__dirname, 'node_modules/.bin/ts-node-dev');
 
 module.exports = {
   apps: [
     {
       name: 'snack-website',
-      script: './src/server/index.tsx',
+      script: `./src/server/index.tsx`,
       interpreter,
-      interpreter_args: '--require tsconfig-paths/register',
+      interpreter_args: '--require tsconfig-paths/register --inspect=9211',
       cwd: 'website',
-      watch: ['.'],
-      watch_delay: 1000,
-      ignore_watch: ['node_modules'],
     },
-    /* {
-      name: 'snack-bundler',
-      script: './src/index.ts',
-      interpreter,
-      watch: true,
-      cwd: 'bundler',
-    }, */
     {
       name: 'exp-www-proxy',
-      script: './src/index.ts',
+      script: `./src/index.ts`,
       interpreter,
-      watch: true,
+      interpreter_args: '--inspect=9220',
       cwd: 'packages/expo-www-proxy',
     },
     {
       name: 'exp-web-proxy',
-      script: './src/index.ts',
+      script: `./src/index.ts`,
       interpreter,
-      watch: true,
+      interpreter_args: '--inspect=9221',
       cwd: 'packages/expo-website-proxy',
     },
+    /* {
+      name: 'snack-bundler-proxy',
+      script: './src/index.ts',
+      interpreter,
+      interpreter_args: '--inspect=9222',
+      cwd: 'packages/snack-bundler-proxy',
+    }, */
   ],
 };
