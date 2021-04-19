66eaf4a705af9f514103e7309a48ec74901aed20
Cedric van Putten <me@bycedric.com>
2021-04-19 15:35:20 +0200

    [website] Enable SDK 41 in version selector

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -8,7 +8,7 @@ export const versions: { [version: string]: boolean } = {
   '38.0.0': true,
   '39.0.0': true,
   '40.0.0': true,
-  '41.0.0': false,
+  '41.0.0': true,
 };
 
 export const DEFAULT_SDK_VERSION = '40.0.0';
