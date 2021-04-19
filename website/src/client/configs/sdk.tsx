d521e81ae8c2f29464af909d21762ec404e0c825
Cedric van Putten <me@bycedric.com>
2021-04-19 15:34:56 +0200

    [website] Fix sdkVersion flag for the website

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -1,6 +1,6 @@
 /**
  * Setting the value to `false` will hide the version from the version picker.
- * It can be selected by specifying `&version=99.0.0` in the URL. Ex: http://snack.expo.io?version=99.0.0
+ * It can be selected by specifying `?sdkVersion=99.0.0` in the URL. Ex: http://snack.expo.io?sdkVersion=99.0.0
  * This is useful when deploying the website  with preliminary support for a new SDK version.
  */
 export const versions: { [version: string]: boolean } = {
