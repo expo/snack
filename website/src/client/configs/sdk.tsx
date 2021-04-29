ad20a1f5864772edb8501790b2c8ac6a0217d7b5
Hein Rutjes <hrutjes@gmail.com>
2021-04-29 09:46:59 +0200

    [website] Use Expo SDK 41 by default

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -10,5 +10,5 @@ export const versions: { [version: string]: boolean } = {
   '41.0.0': true,
 };
 
-export const DEFAULT_SDK_VERSION = '40.0.0';
+export const DEFAULT_SDK_VERSION = '41.0.0';
 export const TEST_SDK_VERSION = '38.0.0';
