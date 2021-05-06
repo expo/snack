aa4a5d3ca3292d8b1124ed9c3784ead09f2ef714
Hein Rutjes <hrutjes@gmail.com>
2021-05-06 14:41:44 +0200

    fix website dev host

--- website/src/client/components/App.tsx
+++ website/src/client/components/App.tsx
@@ -215,7 +215,7 @@ class Main extends React.Component<Props, State> {
         // Use staging server in development, otherwise Expo Go and appetize
         // can't access the runtime. Replace with ngrok url to test locally.
         process.env.NODE_ENV === 'development'
-          ? 'staging.snack.exp.host'
+          ? 'staging.exp.host'
           : new URL(nullthrows(process.env.API_SERVER_URL)).host,
       webPreviewRef: typeof window !== 'undefined' ? this._previewRef : undefined,
       // Serve local web-player through `/web-player` end-point to prevent CORS issues
