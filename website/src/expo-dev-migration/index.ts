c64dd2fbd82e06ad415119a510c8bfb11887f5eb
Juwan Wheatley <datwheat@gmail.com>
2021-06-22 16:56:50 -0400

    [website][debug] add debug code to figure out why .dev transition isn't working in staging

--- website/src/expo-dev-migration/index.ts
+++ website/src/expo-dev-migration/index.ts
@@ -3,24 +3,37 @@ import { Context } from 'koa';
 export function isDevDomainEnabled(): boolean {
   const DEPLOY_ENVIRONMENT = process.env.DEPLOY_ENVIRONMENT;
 
+  console.log('DEPLOY_ENVIRONMENT', DEPLOY_ENVIRONMENT);
+
   if (!DEPLOY_ENVIRONMENT) return false;
 
-  if (['development', 'staging'].includes(DEPLOY_ENVIRONMENT)) return true;
+  if (['staging'].includes(DEPLOY_ENVIRONMENT)) return true;
   if (['production'].includes(DEPLOY_ENVIRONMENT)) return false;
 
   return false;
 }
 
 export function redirectToDevDomain(ctx: Context): boolean {
+  console.log(`${ctx.protocol}://${ctx.hostname}`);
+
+  console.log('isDevDomainEnabled', isDevDomainEnabled());
+  console.log('ctx.protocol', ctx.protocol);
+  console.log('ctx.hostname', ctx.hostname);
+  console.log('ctx.req.url', ctx.req.url);
+  console.log('process.env.LEGACY_SNACK_SERVER_URL', process.env.LEGACY_SNACK_SERVER_URL);
+  console.log('process.env.SNACK_SERVER_URL', process.env.SNACK_SERVER_URL);
+
   // if the incoming request is from snack.expo.io, redirect to snack.expo.dev
   if (
     isDevDomainEnabled() &&
     `${ctx.protocol}://${ctx.hostname}` === process.env.LEGACY_SNACK_SERVER_URL &&
     process.env.SNACK_SERVER_URL
   ) {
+    console.log('is redirecting');
     ctx.redirect(`${process.env.SNACK_SERVER_URL}${ctx.req.url}`);
     return true;
   } else {
+    console.log('is not redirecting');
     return false;
   }
 }
