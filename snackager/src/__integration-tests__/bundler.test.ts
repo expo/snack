64305e10cdf459e2644d4f6b99cde7a10b699aea
Cedric van Putten <me@bycedric.com>
2021-06-25 14:25:40 +0200

    [snackager] Add graphql 15 test and validate externals

--- snackager/src/__integration-tests__/bundler.test.ts
+++ snackager/src/__integration-tests__/bundler.test.ts
@@ -199,5 +199,24 @@ describe('bundler', () => {
     // see: https://github.com/graphql/graphql-js/issues/1272
     const bundle = await bundleAsync('graphql@14.0.0');
     expect(bundle).toMatchSnapshot();
+    // Also validate that we don't have any relative extenals
+    ['android', 'ios', 'web'].forEach(platform => {
+      expect(bundle.files[platform]['bundle.js'].externals).not.toEqual(
+        expect.arrayContaining([expect.stringContaining('./')])
+      );
+    });
+  });
+
+  it('creates bundle for graphql@15.5.1', async () => {
+    // This version of graphql contains a fix for the issue in graphql@14.
+    // In this version, mjs files have the `.mjs` extension when imported.
+    const bundle = await bundleAsync('graphql@15.5.1');
+    expect(bundle).toMatchSnapshot();
+    // Also validate that we don't have any relative extenals
+    ['android', 'ios', 'web'].forEach(platform => {
+      expect(bundle.files[platform]['bundle.js'].externals).not.toEqual(
+        expect.arrayContaining([expect.stringContaining('./')])
+      );
+    });
   });
 });
