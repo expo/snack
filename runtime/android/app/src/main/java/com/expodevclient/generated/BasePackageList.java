712141288e8c91bab65f6456d9f4899e1b861b27
Cedric van Putten <me@bycedric.com>
2021-07-22 13:56:52 +0200

    [runtime] Support running Snack runtime as devclient
    This allows everyone to customize the native dependencies within Snack.

---
+++ runtime/android/app/src/main/java/com/expodevclient/generated/BasePackageList.java
@@ -0,0 +1,23 @@
+package com.expodevclient.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.analytics.amplitude.AmplitudePackage(),
+        new expo.modules.application.ApplicationPackage(),
+        new expo.modules.barcodescanner.BarCodeScannerPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.updates.UpdatesPackage()
+    );
+  }
+}
