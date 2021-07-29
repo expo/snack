02a594e8207a630b1f5b6436fc0f12408d925ee3
Juwan Wheatley <datwheat@gmail.com>
2021-07-29 18:36:21 -0400

    remove .dev conditional

--- website/src/expo-dev-migration/index.ts
+++ website/src/expo-dev-migration/index.ts
@@ -1,19 +1,8 @@
 import { Context } from 'koa';
 
-export function isDevDomainEnabled(): boolean {
-  const DEPLOY_ENVIRONMENT = process.env.DEPLOY_ENVIRONMENT;
-
-  if (!DEPLOY_ENVIRONMENT) return false;
-
-  if (['staging', 'production'].includes(DEPLOY_ENVIRONMENT)) return true;
-
-  return false;
-}
-
 export function redirectToDevDomain(ctx: Context): boolean {
   // if the incoming request is from snack.expo.io, redirect to snack.expo.dev
   if (
-    isDevDomainEnabled() &&
     `${ctx.protocol}://${ctx.hostname}` === process.env.LEGACY_SNACK_SERVER_URL &&
     process.env.SNACK_SERVER_URL
   ) {
