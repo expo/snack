85d04250ea574f98719a191e8afa76551e625420
Cedric van Putten <me@bycedric.com>
2021-09-03 23:21:04 +0200

    [snackager] Reformat reanimated plugin fix for motify

--- snackager/src/utils/packageBundle.ts
+++ snackager/src/utils/packageBundle.ts
@@ -121,7 +121,8 @@ async function packageBundleUnsafe({
       // need to be converted.
       reanimatedPlugin:
         (pkg.name === 'react-native-reanimated' && pkg.version >= '2') ||
-        pkg.name === 'moti' || pkg.name.startsWith('@motify/') ||
+        pkg.name === 'moti' || 
+        pkg.name.startsWith('@motify/') ||
         externals.includes('react-native-reanimated'),
     })
   );
