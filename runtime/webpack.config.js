622fa63f6081e0c4b78837a3d6817ceb179c7634
Cedric van Putten <me@bycedric.com>
2022-01-05 13:41:01 +0100

    [runtime] Update project files from blank template

--- runtime/webpack.config.js
+++ runtime/webpack.config.js
@@ -17,9 +17,8 @@ module.exports = async function (env, argv) {
   // By default `publicPath` is set to '/' which breaks our links
   config.output.publicPath = '';
 
-  config.plugins.find(
-    (p) => p.constructor.name === 'HtmlWebpackPlugin'
-  ).options.favicon = path.join(__dirname, 'assets', 'favicon.png');
+  config.plugins.find((p) => p.constructor.name === 'HtmlWebpackPlugin').options.favicon =
+    path.join(__dirname, 'assets', 'favicon.png');
 
   config.plugins.find(
     (p) => p.constructor.name === 'InterpolateHtmlPlugin'
