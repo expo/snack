c3cb4b61b1a53cead27541a84984148451f916d6
Cedric van Putten <me@bycedric.com>
2022-01-05 18:36:23 +0100

    [website] Sanitize theme name to light or dark only (#244)
    * [website] Sanitize theme name to light or dark only
    
    Without this, the syntax highlighting might be wrong when for null theme values.
    
    * [website] Fix linting issue

--- website/src/client/components/Preferences/withThemeName.tsx
+++ website/src/client/components/Preferences/withThemeName.tsx
@@ -10,6 +10,10 @@ type InjectedProps = {
   theme: ThemeName;
 };
 
+function sanitizeThemeName(theme?: ThemeName | null): ThemeName {
+  return theme === 'dark' ? 'dark' : 'light';
+}
+
 // react-redux doesn't work with forwardRef: https://github.com/reduxjs/react-redux/issues/914
 // so this HOC always needs wrap a connect call, and a connect call cannot wrap this
 export default function withThemeName<P extends InjectedProps>(
@@ -24,9 +28,10 @@ export default function withThemeName<P extends InjectedProps>(
 
       return (
         <PreferencesContext.Consumer>
-          {(props) => {
+          {(props: any) => {
+            const theme = sanitizeThemeName(props.preferences.theme);
             // @ts-ignore
-            return <Comp ref={__forwardedRef} theme={props.preferences.theme} {...rest} />;
+            return <Comp ref={__forwardedRef} theme={theme} {...rest} />;
           }}
         </PreferencesContext.Consumer>
       );
