cd9461c64942b37a9cd86a568440223d877c8e5c
James Ide <ide@expo.dev>
2022-02-24 11:46:19 -0800

    [snack-content] Add back detection for all features

--- packages/snack-content/src/sdks/features.ts
+++ packages/snack-content/src/sdks/features.ts
@@ -1,6 +1,13 @@
 import { SDKFeature } from './types';
 
 // Minimum SDK versions that support Snack features
-const features: { [feature in SDKFeature]: string } = { TYPESCRIPT: '31.0.0' };
+const features: { [feature in SDKFeature]: string } = {
+  MULTIPLE_FILES: '21.0.0',
+  PROJECT_DEPENDENCIES: '25.0.0',
+  TYPESCRIPT: '31.0.0',
+  UNIMODULE_IMPORTS: '33.0.0',
+  POSTMESSAGE_TRANSPORT: '35.0.0',
+  VERSIONED_SNACKAGER: '37.0.0',
+};
 
 export default features;
