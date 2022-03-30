9e5e79a713d2a9ee77f3264ff10c325701dcf7c6
Cedric van Putten <me@bycedric.com>
2022-03-30 19:38:46 +0200

    [snack-sdk] Add the sdk version tests back after incorrect conflict resolve

--- packages/snack-sdk/src/__tests__/sdkversion-test.ts
+++ packages/snack-sdk/src/__tests__/sdkversion-test.ts
@@ -1,17 +1,61 @@
-import { Snack, defaultConfig } from '..';
+import '../__mocks__/fetch';
+import { getSupportedSDKVersions, newestSdkVersion, oldestSdkVersion } from 'snack-content';
 
-switch (process.env.SNACK_ENV) {
-  case 'local':
-    defaultConfig.apiURL = 'http://localhost:3000';
-    defaultConfig.snackagerURL = 'https://localhost:3012';
-    break;
-  case 'staging':
-    defaultConfig.apiURL = 'https://staging.exp.host';
-    defaultConfig.snackagerURL = 'https://staging.snackager.expo.io';
-    break;
-}
+import Snack, { defaultConfig } from './snack-sdk';
 
-export * from '..';
-export default Snack;
+describe('sdkVersion', () => {
+  it('uses default when omitted', async () => {
+    const snack = new Snack({});
+    expect(snack.getState()).toMatchObject({
+      sdkVersion: defaultConfig.sdkVersion,
+    });
+  });
 
-test('skip', () => {});
+  it('can be provided initially', async () => {
+    const snack = new Snack({ sdkVersion: newestSdkVersion });
+    expect(snack.getState()).toMatchObject({
+      unsaved: false,
+      sdkVersion: newestSdkVersion,
+    });
+  });
+
+  it('can be changed', async () => {
+    const snack = new Snack({ sdkVersion: oldestSdkVersion });
+    snack.setSDKVersion(newestSdkVersion);
+    expect(snack.getState()).toMatchObject({
+      unsaved: true,
+      sdkVersion: newestSdkVersion,
+    });
+  });
+
+  it('throws when invalid version specified', async () => {
+    expect(
+      () =>
+        new Snack({
+          // @ts-ignore Type '"30.0.0"' is not assignable to type 'SDKVersion'
+          sdkVersion: '30.0.0',
+        })
+    ).toThrowError();
+  });
+
+  it('throws when invalid version specified to setSDKVersion', async () => {
+    const snack = new Snack({});
+    expect(() =>
+      // @ts-ignore Type '"30.0.0"' is not assignable to type 'SDKVersion'
+      snack.setSDKVersion('thisaintright')
+    ).toThrowError();
+  });
+
+  getSupportedSDKVersions().forEach((sdkVersion) => {
+    it(`fetches wantedDependencyVersions for SDK ${sdkVersion}`, async () => {
+      const snack = new Snack({
+        sdkVersion,
+        dependencies: {
+          'expo-blur': { version: '*' },
+        },
+      });
+      const { wantedDependencyVersions } = await snack.getStateAsync();
+      expect(Object.keys(wantedDependencyVersions ?? {}).length).toBeGreaterThan(0);
+    });
+  });
+});
