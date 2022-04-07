01586bcde7efe5fa2e6d0119c291075660b99c4c
Cedric van Putten <me@bycedric.com>
2022-04-07 14:11:09 +0200

    fix: add missing root to all configs

--- packages/snack-content/.eslintrc.js
+++ packages/snack-content/.eslintrc.js
@@ -1,6 +1,7 @@
 const { jsExtensions, tsExtensions } = require('eslint-config-universe/shared/extensions');
 
 module.exports = {
+  root: true,
   extends: 'universe/node',
   // This project does not contain babel, force all files to use the typescript parser instead.
   parser: '@typescript-eslint/parser',
--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   extends: ['universe/node', 'universe/web'],
   parserOptions: {
     project: ['./tsconfig.json'],
