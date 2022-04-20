61edb71069a8135b210913abd255c8058df98bd1
Cedric van Putten <me@bycedric.com>
2022-04-20 17:57:15 +0200

    [packages] Prepare snack-sdk and snack-content for SDK 44 (#277)
    * [packages] Prepare snack-sdk and snack-content for SDK 44
    
    * [snack-sdk] Update tests and snapshots

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -1,6 +1,6 @@
 import { SDKVersion } from './types';
 
-export const defaultSdkVersion: SDKVersion = '43.0.0';
+export const defaultSdkVersion: SDKVersion = '44.0.0';
 
 // Mostly used for tests
 export const oldestSdkVersion: SDKVersion = '41.0.0';
