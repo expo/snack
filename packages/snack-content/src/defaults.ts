9f1fa84f736052783e93ba478341f22b1ef05c24
Cedric van Putten <me@bycedric.com>
2022-05-19 22:40:41 +0200

    [website] Set default back to sdk 44 and centralize switch (#295)
    * [snackager] Hide babel peer dependencies from dependency list (#293)
    
    * [sdk] Update snapshot with default SDK 44
    
    * [snackager] Update snapshot tests with sdk 44

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -1,6 +1,6 @@
 import { SDKVersion } from './types';
 
-export const defaultSdkVersion: SDKVersion = '45.0.0';
+export const defaultSdkVersion: SDKVersion = '44.0.0';
 
 // Mostly used for tests
 export const oldestSdkVersion: SDKVersion = '43.0.0';
