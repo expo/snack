1cce5c4aa68b77a790f631edddc21c2a05f96093
Cedric van Putten <me@bycedric.com>
2022-05-19 19:46:36 +0200

    [snackager] Hide babel peer dependencies from dependency list (#293)

--- snackager/src/utils/__tests__/resolveDependencies.test.ts
+++ snackager/src/utils/__tests__/resolveDependencies.test.ts
@@ -1,13 +1,14 @@
 import { Metadata } from '../../types';
 import resolveDependencies from '../resolveDependencies';
 
-const metaFixture: Metadata = {
+const fixture: Metadata = {
   name: '<package>',
   'dist-tags': {
     latest: '1.0.0',
     'vector-icons': '1.1.0',
     'windows-dep': '2.0.0',
     'windows-peer': '2.1.0',
+    'babel-peer': '3.0.0',
   },
   versions: {
     '1.0.0': { name: '<package>', version: '1.0.0', dist: {} as any },
@@ -30,37 +31,49 @@ const metaFixture: Metadata = {
       dist: {} as any,
       peerDependencies: { 'react-native-windows': 'latest' },
     },
+    '3.0.0': {
+      name: '<package>',
+      version: '3.0.0',
+      dist: {} as any,
+      peerDependencies: { '@babel/core': 'latest' },
+    },
   },
 };
 
 it('returns non-latest version', () => {
-  expect(resolveDependencies(metaFixture, '1.0.1', false)).toMatchObject({
-    pkg: metaFixture.versions['1.0.1'],
+  expect(resolveDependencies(fixture, '1.0.1', false)).toMatchObject({
+    pkg: fixture.versions['1.0.1'],
     hash: '<package>@1.0.1',
     latestHash: null,
   });
 });
 
 it('returns latest version and deep module', () => {
-  expect(resolveDependencies(metaFixture, '1.0.0', true, 'lib/some/module')).toMatchObject({
-    pkg: metaFixture.versions['1.0.0'],
+  expect(resolveDependencies(fixture, '1.0.0', true, 'lib/some/module')).toMatchObject({
+    pkg: fixture.versions['1.0.0'],
     hash: '<package>~lib~some~module@1.0.0',
     latestHash: '<package>~lib~some~module@latest',
   });
 });
 
 it('adds `@expo/vector-icons` instead of `react-native-vector-icons` as peer dependency', () => {
-  const result = resolveDependencies(metaFixture, metaFixture['dist-tags']['vector-icons'], true);
-  expect(result).toHaveProperty('dependencies.@expo/vector-icons');
-  expect(result).not.toHaveProperty('dependencies.react-native-vector-icons');
+  const result = resolveDependencies(fixture, fixture['dist-tags']['vector-icons'], true);
+  expect(result.dependencies).toHaveProperty('@expo/vector-icons');
+  expect(result.dependencies).not.toHaveProperty('react-native-vector-icons');
 });
 
 it('ignores `react-native-windows` as dependency', () => {
-  const result = resolveDependencies(metaFixture, metaFixture['dist-tags']['windows-dep'], false);
-  expect(result).not.toHaveProperty('dependencies.react-native-windows');
+  const result = resolveDependencies(fixture, fixture['dist-tags']['windows-dep'], false);
+  expect(result.dependencies).not.toHaveProperty('react-native-windows');
 });
 
 it('ignores `react-native-windows` as peer dependency', () => {
-  const result = resolveDependencies(metaFixture, metaFixture['dist-tags']['windows-peer'], false);
-  expect(result).not.toHaveProperty('dependencies.react-native-windows');
+  const result = resolveDependencies(fixture, fixture['dist-tags']['windows-peer'], false);
+  expect(result.dependencies).not.toHaveProperty('react-native-windows');
+});
+
+it('hides `@babel/core` as peer dependency', () => {
+  const result = resolveDependencies(fixture, fixture['dist-tags']['babel-peer'], false);
+  expect(result.dependencies).not.toHaveProperty('@babel/core');
+  expect(result.pkg.peerDependencies).not.toHaveProperty('@babel/core');
 });
