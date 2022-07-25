a7c307225f7d510d88265d78975626abdd0a4a67
Cedric van Putten <me@bycedric.com>
2022-07-25 12:36:42 +0200

    [snack-babel-standalone] Finalize the webpack config

--- packages/snack-babel-standalone/webpack.config.js
+++ packages/snack-babel-standalone/webpack.config.js
@@ -1,35 +1,41 @@
+const path = require('path');
 const webpack = require('webpack');
-const { version } = require('@babel/core/package.json');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-module.exports = {
-  entry: './index',
-  mode: 'production',
-  node: {
-    // Mock Node.js modules that Babel require()s but that we don't
-    // particularly care about.
-    fs: 'empty',
-    module: 'empty',
-    net: 'empty',
+const {
+  /** Enable the bundle analyzer to validate the output after updating */
+  WEBPACK_ANALYZE,
+  /** Enable production mode to output the most optimized bundle */
+  WEBPACK_PRODUCTION,
+} = process.env;
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  mode: 'development',
+  entry: {
+    runtime: path.resolve(__dirname, './src/runtime.ts'),
+    eslint: path.resolve(__dirname, './src/eslint.ts'),
   },
   output: {
-    filename: 'babel-standalone.js',
-    library: 'babel-standalone',
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    // library: '[name]', // This causes a weird export
     libraryTarget: 'commonjs2',
-  },
-  devtool: false,
-  optimization: {
-    minimize: true,
+    globalObject: 'this',
   },
   plugins: [
+    new NodePolyfillPlugin(),
     new webpack.DefinePlugin({
-      BABEL_VERSION: JSON.stringify(version),
-      VERSION: JSON.stringify(version),
+      BABEL_VERSION: JSON.stringify(require('@babel/core/package.json').version),
+      VERSION: JSON.stringify(require('@babel/core/package.json').version),
     }),
   ],
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|js|mjs)$/u,
+        exclude: [path.resolve(__dirname, 'node_modules')],
         use: {
           loader: 'babel-loader',
           options: {
@@ -44,7 +50,26 @@ module.exports = {
     ],
   },
   resolve: {
-    // Workaround to disable ESM `module` fields
+    extensions: ['.ts', '.js', '.json'],
     mainFields: ['main'],
-  }
+    fallback: {
+      fs: false,
+    },
+  },
 };
+
+// We want to optimize the bundle output to minimize the size of the bundle
+if (WEBPACK_PRODUCTION || WEBPACK_ANALYZE) {
+  config.mode = 'production';
+  config.devtool = false;
+  config.optimization = {
+    minimize: true,
+  };
+
+  // We want to be able to analyze the bundle size, in case we need to update it
+  if (WEBPACK_ANALYZE) {
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
+}
+
+module.exports = config;
