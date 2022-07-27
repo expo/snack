41e745cdb470505426a3c673d4054673704d3a5a
Cedric van Putten <me@bycedric.com>
2022-07-27 23:49:58 +0200

    [website] Configure jsdom for jest tests

--- website/jest/unit.config.js
+++ website/jest/unit.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: 'ts-jest',
   rootDir: path.resolve(__dirname, '..'),
   testRegex: '/__tests__/.*\\.test\\.(js|tsx?)$',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest/unit.setup.js'],
   moduleNameMapper: {
     'snack-sdk': '<rootDir>../packages/snack-sdk/src',
