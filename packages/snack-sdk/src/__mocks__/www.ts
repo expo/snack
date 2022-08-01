e53ff855c6580a166705a13526b359d6afd37df9
Cedric van Putten <me@bycedric.com>
2022-08-01 18:56:20 +0200

    [snack-sdk] Add versioned endpoints for modules (#334)

--- packages/snack-sdk/src/__mocks__/www.ts
+++ packages/snack-sdk/src/__mocks__/www.ts
@@ -33,6 +33,98 @@ export default function createWww() {
       hash,
     };
   });
+  router.get('/--/api/v2/versions/latest', (ctx) => {
+    ctx.body = {
+      data: {
+        sdkVersions: {
+          '44.0.0': {
+            iosClientUrl: 'https://dpq5q02fu5f55.cloudfront.net/Exponent-2.23.2.tar.gz',
+            releaseNoteUrl: 'https://blog.expo.dev/expo-sdk-44-4c4b8306584a',
+            relatedPackages: {
+              jest: '^26.6.3',
+              typescript: '~4.3.5',
+              '@babel/core': '^7.12.9',
+              '@types/react': '~17.0.21',
+              '@types/react-dom': '~17.0.9',
+              'react-native-web': '0.17.1',
+              'babel-preset-expo': '9.0.2',
+              '@types/react-native': '~0.64.12',
+              '@expo/webpack-config': '~0.16.2',
+              'react-native-unimodules': '~0.15.0',
+            },
+            androidClientUrl: 'https://d1ahtucjixef4r.cloudfront.net/Exponent-2.23.2.apk',
+            iosClientVersion: '2.23.2',
+            expoReactNativeTag: 'sdk-44.0.0',
+            androidClientVersion: '2.23.2',
+            facebookReactVersion: '17.0.1',
+            facebookReactNativeVersion: '0.64.3',
+            packagesToInstallWhenEjecting: {
+              'react-native': 'https://github.com/expo/react-native/archive/sdk-44.0.0.tar.gz',
+              'react-native-unimodules': '0.15.0',
+            },
+          },
+          '45.0.0': {
+            iosClientUrl: 'https://dpq5q02fu5f55.cloudfront.net/Exponent-2.24.3.tar.gz',
+            releaseNoteUrl: 'https://blog.expo.dev/expo-sdk-45-f4e332954a68',
+            relatedPackages: {
+              jest: '^26.6.3',
+              typescript: '~4.3.5',
+              '@babel/core': '^7.12.9',
+              '@expo/config': '~6.0.19',
+              '@types/react': '~17.0.21',
+              '@types/react-dom': '~17.0.11',
+              'react-native-web': '0.17.7',
+              'babel-preset-expo': '~9.1.0',
+              '@types/react-native': '~0.67.6',
+              '@expo/config-plugins': '^4.1.0',
+              '@expo/webpack-config': '~0.16.21',
+              '@expo/prebuild-config': '^4.0.0',
+              'expo-modules-autolinking': '~0.8.1 || ~0.9.0',
+            },
+            androidClientUrl: 'https://d1ahtucjixef4r.cloudfront.net/Exponent-2.24.6.apk',
+            iosClientVersion: '2.24.3',
+            expoReactNativeTag: 'sdk-45.0.0',
+            androidClientVersion: '2.24.6',
+            facebookReactVersion: '17.0.2',
+            facebookReactNativeVersion: '0.68.2',
+            packagesToInstallWhenEjecting: {
+              'react-native': 'https://github.com/expo/react-native/archive/sdk-45.0.0.tar.gz',
+              'react-native-unimodules': '0.15.0',
+            },
+          },
+          '46.0.0': {
+            beta: 'true',
+            iosClientUrl: 'https://dpq5q02fu5f55.cloudfront.net/Exponent-2.25.1.tar.gz',
+            relatedPackages: {
+              jest: '^26.6.3',
+              typescript: '^4.6.3',
+              '@babel/core': '^7.18.6',
+              '@expo/config': '^7.0.0',
+              '@types/react': '~18.0.0',
+              '@types/react-dom': '~18.0.0',
+              'react-native-web': '~0.18.7',
+              'babel-preset-expo': '~9.2.0',
+              '@types/react-native': '~0.69.1',
+              '@expo/config-plugins': '^5.0.0',
+              '@expo/webpack-config': '^0.17.0',
+              '@expo/prebuild-config': '^5.0.1',
+              'expo-modules-autolinking': '~0.10.1',
+            },
+            androidClientUrl: 'https://d1ahtucjixef4r.cloudfront.net/Exponent-2.25.1.apk',
+            iosClientVersion: '2.25.1',
+            expoReactNativeTag: 'sdk-46.0.0',
+            androidClientVersion: '2.25.1',
+            facebookReactVersion: '18.0.0',
+            facebookReactNativeVersion: '0.69.3',
+            packagesToInstallWhenEjecting: {
+              'react-native': 'https://github.com/expo/react-native/archive/sdk-46.0.0.tar.gz',
+              'react-native-unimodules': '0.15.0',
+            },
+          },
+        },
+      },
+    };
+  });
   app.use(router.routes()).use(router.allowedMethods());
   return app;
 }
