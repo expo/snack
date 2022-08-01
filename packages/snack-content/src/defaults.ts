eb3be3e930410b85f00e4dcc01361e738a41216c
Cedric van Putten <me@bycedric.com>
2022-08-01 19:36:02 +0200

    Set default SDK back to 44 (#344)
    * [website] Add type failure warning and vendored types
    
    * [website] Fix eslint errors
    
    * [website] Fix react/jsx-runtime types and remove react import from defaults
    
    * [website] Add back import react for SDK <46 support
    
    * [website] Add type failure warning and vendored types
    
    * [website] Fix eslint errors
    
    * [website] Add type failure warning and vendored types
    
    * [website] Fix eslint errors
    
    * Set default SDK back to 44

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -1,6 +1,6 @@
 import { SDKVersion } from './types';
 
-export const defaultSdkVersion: SDKVersion = '46.0.0';
+export const defaultSdkVersion: SDKVersion = '44.0.0';
 
 // Mostly used for tests
 export const oldestSdkVersion: SDKVersion = '44.0.0';
