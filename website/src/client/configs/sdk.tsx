133f0f3c4d133151403639813b9fe7fc3766c28f
Cedric van Putten <me@bycedric.com>
2022-11-02 19:14:11 +0100

    refactor(website): upgrade to Expo SDK 47 beta

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -7,10 +7,10 @@ import { SDKVersion } from 'snack-sdk';
  * This is useful when deploying the website  with preliminary support for a new SDK version.
  */
 export const versions: Record<SDKVersion, boolean> = {
-  '44.0.0': true,
   '45.0.0': true,
   '46.0.0': true,
+  '47.0.0': true,
 };
 
 export const DEFAULT_SDK_VERSION: SDKVersion = defaultSdkVersion;
-export const TEST_SDK_VERSION: SDKVersion = '44.0.0';
+export const TEST_SDK_VERSION: SDKVersion = '45.0.0';
