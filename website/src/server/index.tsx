d49542d6780b8c56b7681fbe7889e59fa28f4633
Nick Novitski <github@nicknovitski.com>
2022-11-14 11:24:58 -0800

    [website] Don't use stoppable
    When running in kubernetes, this doesn't help us, and when running with
    Google Front End servers, it actively harms availability.

--- website/src/server/index.tsx
+++ website/src/server/index.tsx
@@ -7,7 +7,6 @@ import { AddressInfo } from 'net';
 import nullthrows from 'nullthrows';
 import path from 'path';
 import Raven from 'raven';
-import stoppable from 'stoppable';
 
 import routes from './routes';
 import sw from './sw';
@@ -125,7 +124,7 @@ app.use(bodyParser());
 app.use(routes());
 
 const httpServer = app.listen(port, host, backlog, () => {
-  const { address, port } = server.address() as AddressInfo;
+  const { address, port } = httpServer.address() as AddressInfo;
 
   console.log(
     `The Snack web server is listening on http://${address}:${port}, took ${
@@ -136,24 +135,18 @@ const httpServer = app.listen(port, host, backlog, () => {
 
 httpServer.timeout = timeout;
 
-// In development, it's common to stop or restart the server so we immediately end and close all
-// sockets when stopping the server instead of waiting for the requests to finish. In production,
-// we allow the requests a grace period to complete before ending and closing the sockets.
-const gracePeriod = process.env.NODE_ENV === 'development' ? 0 : httpServer.timeout;
-const server = stoppable(httpServer, gracePeriod);
-
 // Listen to HTTP server error events and handle shutting down the server gracefully
 let exitSignal: ShutdownSignal | null = null;
 let httpServerError: Error | null = null;
 
-server.on('error', (error) => {
+httpServer.on('error', (error) => {
   httpServerError = error;
   console.error(`There was an error with the HTTP server:`, error);
   console.error(`The HTTP server is shutting down and draining existing connections`);
-  server.stop();
+  httpServer.close();
 });
 
-server.on('close', () => {
+httpServer.on('close', () => {
   console.log(`The HTTP server has drained all connections and is scheduling its exit`);
   console.log(`The HTTP server process is exiting...`);
   // Let other "close" event handlers run before exiting
@@ -171,7 +164,7 @@ const shutdown = (signal: ShutdownSignal) => {
     `Received ${signal}; the HTTP server is shutting down and draining existing connections`
   );
   exitSignal = signal;
-  server.stop();
+  httpServer.close();
 };
 
 // TODO: In Node 9, the signal is passed as the first argument to the listener
