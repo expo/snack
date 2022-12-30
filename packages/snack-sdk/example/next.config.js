7a6084a3ddad569ec153b1f659332596a2d82c4f
Kudo Chien <kudo@expo.dev>
2022-12-31 02:03:59 +0800

    [snack-sdk] fix example 404 page not found

--- packages/snack-sdk/example/next.config.js
+++ packages/snack-sdk/example/next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.alias['snack-sdk'] = path.resolve(__dirname, '../../../packages/snack-sdk');
+    config.resolve.alias['vm2'] = false;
     return config;
   },
 };
