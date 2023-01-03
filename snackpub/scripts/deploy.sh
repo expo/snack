2c499d16caab8e711ba6b4d84f92a24e92231a35
Kudo Chien <kudo@expo.dev>
2023-01-03 13:40:49 +0800

    [snackpub] add deploy script

---
+++ snackpub/scripts/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -eo pipefail
+
+usage() {
+  echo "Usage: $0 -e <environment>"
+  exit 1
+}
+
+while getopts "e:" option; do
+  case $option in
+    e)
+      environment=$OPTARG
+      if [ "$environment" != 'staging' ] && [ "$environment" != 'production' ]; then
+        echo "Invalid environment: $environment"
+        exit 1
+      fi
+      ;;
+    *)
+      usage
+      ;;
+   esac
+done
+
+if [ -z "$environment" ]; then
+  usage
+elif [ "$environment" = 'staging' ]; then
+  service_name='staging-snackpub'
+  env_vars_file='secrets/staging/snackpub.env.yaml'
+elif [ "$environment" = 'production' ]; then
+  service_name='snackpub'
+  env_vars_file='secrets/production/snackpub.env.yaml'
+fi
+
+printf "\nDeploying to Google Cloud Platform...\n"
+
+gcloud run deploy "$service_name" \
+  --image=gcr.io/exponentjs/snackpub:latest \
+  --port=3013 \
+  --allow-unauthenticated \
+  --timeout=1800 \
+  --concurrency=1000 \
+  --max-instances=10 \
+  --env-vars-file="$env_vars_file"
+
+printf "\nCloud Run $service_name has been deployed"
