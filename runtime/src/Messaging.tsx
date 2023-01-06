aaf73c7cdc83c3ea6105debf51b3b7993658629c
Kudo Chien <kudo@expo.dev>
2023-01-06 20:32:56 +0800

    Apply suggestions from code review
    Co-authored-by: Cedric van Putten <me@bycedric.com>

--- runtime/src/Messaging.tsx
+++ runtime/src/Messaging.tsx
@@ -33,28 +33,28 @@ export const init = (deviceId: string, testTransport: string | null | undefined)
 
 export const unsubscribe = () => {
   if (!transport) {
-    throw new Error('Transport does not initialize. Please call `Messaging.init()` first.');
+    throw new Error('Transport not initialized, call `Messaging.init()` first.');
   }
   transport.unsubscribe();
 };
 
 export const subscribe = ({ channel }: { channel: string }) => {
   if (!transport) {
-    throw new Error('Transport does not initialize. Please call `Messaging.init()` first.');
+    throw new Error('Transport not initialized, call `Messaging.init()` first.');
   }
   transport.subscribe(channel);
 };
 
 export const listen = (listener: (payload: RuntimeMessagePayload) => void) => {
   if (!transport) {
-    throw new Error('Transport does not initialize. Please call `Messaging.init()` first.');
+    throw new Error('Transport not initialized, call `Messaging.init()` first.');
   }
   transport.listen(listener);
 };
 
 export const publish = (message: object) => {
   if (!transport) {
-    throw new Error('Transport does not initialize. Please call `Messaging.init()` first.');
+    throw new Error('Transport not initialized, call `Messaging.init()` first.');
   }
   transport.publish(message);
 };
