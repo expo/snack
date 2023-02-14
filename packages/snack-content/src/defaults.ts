0e48954274162b9357e3c19deed8861f694a71dc
Cedric van Putten <me@bycedric.com>
2023-02-14 18:58:24 +0100

    chore: use expo sdk 47 by default (#390)
    * chore(snack-content): use expo sdk 47 by default
    
    * fix(snack-sdk): use proper snack-content version range
    
    * test(snack-sdk): update snapshot tests for new default sdk
    
    * test(snackager): update snapshot tests for new default sdk

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -1,6 +1,6 @@
 import { SDKVersion } from './types';
 
-export const defaultSdkVersion: SDKVersion = '46.0.0';
+export const defaultSdkVersion: SDKVersion = '47.0.0';
 
 // Mostly used for tests
 export const oldestSdkVersion: SDKVersion = '45.0.0';
