dd67f112edc3a150f01dfb6c460b36eab8d633a1
Kudo Chien <kudo@expo.dev>
2023-03-11 23:07:57 +0800

    [snackpub] introduce NetworkUtils.getRemoteAddress

---
+++ snackpub/src/NetworkUtils.ts
@@ -0,0 +1,12 @@
+import type { IncomingHttpHeaders, IncomingMessage } from 'http';
+
+type ValueOf<T> = T[keyof T];
+
+export function getRemoteAddress(req: IncomingMessage): string | undefined {
+  return getFirstHeaderValue(req.headers['x-forwarded-for']) ?? req.socket.remoteAddress;
+}
+
+function getFirstHeaderValue(headerValue: ValueOf<IncomingHttpHeaders>): string | undefined {
+  const firstValue = Array.isArray(headerValue) ? headerValue[0] : headerValue;
+  return firstValue?.split(/\s*,\s*/, 1)[0];
+}
