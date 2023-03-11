0fdc5536c0eccd69e66a77fb20936cfb563c5466
Kudo Chien <kudo@expo.dev>
2023-03-11 14:56:29 +0800

    [snackpub] handle redis connection error and reconnection

--- snackpub/src/RedisAdapter.ts
+++ snackpub/src/RedisAdapter.ts
@@ -1,22 +1,14 @@
 import { createAdapter } from '@socket.io/redis-adapter';
-import assert from 'assert';
 import type { createClient } from 'redis';
 import type { Server } from 'socket.io';
-
-let subClient: ReturnType<typeof createClient> | null = null;
 
 export async function bindRedisAdapterAsync(
   server: Server,
   redisClient: ReturnType<typeof createClient>
-) {
-  assert(subClient == null, 'Found an orphaned Redis subscription client');
-  subClient = redisClient.duplicate();
-  await subClient.connect();
-  server.adapter(createAdapter(redisClient, subClient, { key: 'snackpub' }));
+): Promise<ReturnType<typeof createClient>> {
+  const redisSubscriptionClient = redisClient.duplicate();
+  await redisSubscriptionClient.connect();
+  server.adapter(createAdapter(redisClient, redisSubscriptionClient, { key: 'snackpub' }));
   console.log('[RedisAdapter] Using redis adapter.');
-}
-
-export async function closeRedisAdapterAsync() {
-  subClient?.quit();
-  subClient = null;
+  return redisSubscriptionClient;
 }
