574e0658f65c52d5869742aeeaceec4b40aa5b11
Aman Mittal <amandeepmittal@live.com>
2023-04-18 18:27:11 +0530

    Update forums.expo.dev link

--- website/src/client/configs/defaults.tsx
+++ website/src/client/configs/defaults.tsx
@@ -100,7 +100,7 @@ When you're ready to see everything that Expo provides (or if you want to use yo
 
 All projects created in Snack are publicly available, so you can easily share the link to this project via link, or embed it on a web page with the \`<>\` button.
 
-If you're having problems, you can tweet to us [@expo](https://twitter.com/expo) or ask in our [forums](https://forums.expo.dev/) or [Discord](https://chat.expo.dev/).
+If you're having problems, you can tweet to us [@expo](https://twitter.com/expo) or ask in our [forums](https://forums.expo.dev/c/expo-dev-tools/61) or [Discord](https://chat.expo.dev/).
 
 Snack is Open Source. You can find the code on the [GitHub repo](https://github.com/expo/snack).
 `,
