0daac6d800f60ff8849b3c1a8e6e45b2e9ad0c07
Cedric van Putten <github@cedric.dev>
2023-05-26 14:35:17 +0200

    refactor(snack-content): upgrade to Expo SDK 48

--- packages/snack-content/src/sdks/index.ts
+++ packages/snack-content/src/sdks/index.ts
@@ -7,36 +7,6 @@ const assets = {
 };
 
 const sdks: { [version: string]: SDKSpec } = {
-  '45.0.0': {
-    version: '^45.0.0',
-    coreModules: {
-      ...assets,
-      expo: '~45.0.0',
-      react: '17.0.1',
-      'react-dom': '*',
-      'react-native': '0.68.2',
-      'react-native-web': '*',
-    },
-    bundledModules: {
-      'expo-asset': '*',
-      'expo-font': '*',
-      'react-native-gesture-handler': '*',
-      'react-native-safe-area-context': '*',
-      '@expo/vector-icons': '*',
-      'expo-constants': '*',
-      'expo-file-system': '*',
-      'expo-updates': '*',
-      '@react-native-async-storage/async-storage': '*',
-    },
-    deprecatedModules: {
-      '@react-native-community/async-storage':
-        'Async Storage has moved to new organization: https://github.com/react-native-async-storage/async-storage',
-      'expo-permissions':
-        'Use permissions getters and requesters in specific modules instead, such as MediaLibrary.getPermissionsAsync() and MediaLibrary.requestPermissionsAsync().',
-      'expo-app-loading':
-        'Use expo-splash-screen directly instead: SplashScreen.preventAutoHideAsync() and SplashScreen.hideAsync().',
-    },
-  },
   '46.0.0': {
     version: '^46.0.0',
     coreModules: {
@@ -93,6 +63,33 @@ const sdks: { [version: string]: SDKSpec } = {
       'expo-file-system': '*',
       'expo-updates': '*',
       '@react-native-async-storage/async-storage': '*',
+    },
+    deprecatedModules: {},
+  },
+  '48.0.0': {
+    version: '^48.0.0',
+    coreModules: {
+      ...assets,
+      expo: '~48.0.17',
+      react: '18.2.0',
+      'react-dom': '*',
+      'react-native': '0.71.8',
+      'react-native-web': '*',
+      // Used by @shopify/react-native-skia, on web only
+      // See runtime/src/NativeModules/ReactNativeSkia.tsx for more info
+      '@shopify/react-native-skia/lib/module/web': '*',
+    },
+    bundledModules: {
+      'expo-asset': '*',
+      'expo-font': '*',
+      'react-native-gesture-handler': '*',
+      'react-native-safe-area-context': '*',
+      '@expo/vector-icons': '*',
+      'expo-constants': '*',
+      'expo-file-system': '*',
+      'expo-updates': '*',
+      '@react-native-async-storage/async-storage': '*',
+      'react-native-reanimated': '*',
     },
     deprecatedModules: {},
   },
