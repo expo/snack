aaeb70b9da3054720fa0dc4197fff790b0f3a735
Cedric van Putten <me@bycedric.com>
2023-06-29 22:20:47 +0200

    fix(runtime): use proper expo-router entrypoint detection (#439)
    * fix(runtime): use proper expo-router entrypoint detection
    
    * fix(runtime): use proper `index` file detection

--- runtime/src/Modules.tsx
+++ runtime/src/Modules.tsx
@@ -120,10 +120,6 @@ export const updateProjectDependencies = async (newProjectDependencies: Dependen
   return changedDependencies.map(sanitizeModule);
 };
 
-export function hasDependency(name: string) {
-  return !!projectDependencies[name];
-}
-
 // SystemJS fetch pipeline
 const _get = (header: { [key: string]: string }, value: string) =>
   header?.hasOwnProperty(value) ? header[value] : null;
