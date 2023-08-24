afc09795e2ab416b88ad451418fa45080877024e
Cedric van Putten <github@cedric.dev>
2023-08-24 16:57:27 +0200

    chore(runtime-shell): clean up index file

--- runtime-shell/index.js
+++ runtime-shell/index.js
@@ -1,16 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-try {
-  const App = require('./src/App').default;
-  // import App from './src/App';
+import App from './src/App';
 
-  console.log('app', App);
-
-  // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-  // It also ensures that whether you load the app in Expo Go or in a native build,
-  // the environment is set up appropriately
-  registerRootComponent(App);
-} catch (error) {
-  console.error(error);
-  throw error;
-}
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
