ab13af2b3e71ab26ae045fa0ac33ae5eed3b1c61
Cedric van Putten <github@cedric.dev>
2023-09-15 14:43:10 +0200

    chore(website): upgrade to `expo@49.0.10`

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -7,10 +7,10 @@ import { SDKVersion } from 'snack-sdk';
  * This is useful when deploying the website  with preliminary support for a new SDK version.
  */
 export const versions: Record<SDKVersion, boolean> = {
-  '46.0.0': true,
   '47.0.0': true,
   '48.0.0': true,
+  '49.0.0': true,
 };
 
 export const DEFAULT_SDK_VERSION: SDKVersion = defaultSdkVersion;
-export const TEST_SDK_VERSION: SDKVersion = '46.0.0';
+export const TEST_SDK_VERSION: SDKVersion = '48.0.0';
