17a299f341053715768e675cff581c981e0ede95
Cedric van Putten <github@cedric.dev>
2023-09-18 18:32:50 +0200

    chore: drop legacy sdk

--- packages/snack-sdk-legacy/example/README.md
+++
@@ -1,3 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
-
-Run using `npm install && npm start`. The important code lives in `src/App.js`.
--- packages/snack-sdk-legacy/src/configs/supportedModules/index.js
+++
@@ -1,7 +0,0 @@
-import { type SDKVersion } from '../sdkVersions';
-
-const supportedVersions: { [key: SDKVersion]: { [key: string]: string } | undefined } = {
-  // No longer supported
-};
-
-export default supportedVersions;
