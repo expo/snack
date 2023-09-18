faee504a211d4096a87ad2293491ba549a4b1028
Cedric van Putten <me@bycedric.com>
2023-09-18 18:35:00 +0200

    chore: drop legacy sdk (#470)

--- packages/snack-sdk-legacy/src/utils/getSupportedVersion.js
+++
@@ -1,9 +0,0 @@
-import { type SDKVersion } from '../configs/sdkVersions';
-import supportedModules from '../configs/supportedModules';
-
-export default function getSupportedVersion(
-  name: string,
-  sdkVersion: SDKVersion
-): string | undefined {
-  return supportedModules[sdkVersion] ? supportedModules[sdkVersion][name] : undefined;
-}
