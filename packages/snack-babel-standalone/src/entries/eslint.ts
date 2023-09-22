3991151a27f4568d8386307e1d7770e1ae72bc00
Cedric van Putten <github@cedric.dev>
2023-09-22 15:17:14 +0200

    fix(snack-babel-standalone): export `traverse` for eslint

--- packages/snack-babel-standalone/src/entries/eslint.ts
+++ packages/snack-babel-standalone/src/entries/eslint.ts
@@ -17,7 +17,7 @@ export const version = babelVersion;
 // The tokTypes is an internal in babel, specifically for the `@babel/eslint-parser`.
 // See: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/b08e4746a2f6321aa20410c00bc615662351f984/types/babel__core/index.d.ts#L724-L725
 // @ts-expect-error Module '"@babel/core"' has no exported member 'tokTypes'.
-export { types, tokTypes } from '@babel/core';
+export { traverse, types, tokTypes } from '@babel/core';
 
 // Babel core's parse sync is used in the `@babel/eslint-parser`.
 // See: https://github.com/babel/babel/blob/v7.14.2/eslint/babel-eslint-parser/src/index.js#L4
