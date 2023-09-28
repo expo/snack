6aa1e63a2f8d6c398cfd04ead74a17a07cdad207
Cedric van Putten <me@bycedric.com>
2023-09-28 15:58:03 +0200

    chore(website): update the iframe permissions when previewing on web (#478)

--- website/src/client/components/DevicePreview/WebFrame.tsx
+++ website/src/client/components/DevicePreview/WebFrame.tsx
@@ -17,7 +17,7 @@ export default function WebFrame({ previewRef, previewURL, onPopupUrl }: Props) 
       <iframe
         ref={(c) => (previewRef.current = c?.contentWindow ?? null)}
         src={previewURL}
-        allow="geolocation; camera; microphone"
+        allow={iframePermissions}
         className={css(styles.frame)}
       />
     </div>
@@ -41,3 +41,27 @@ const styles = StyleSheet.create({
     backgroundColor: c('content', 'light'),
   },
 });
+
+/**
+ * List of all iFrame permission directives.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Permissions-Policy#directives
+ */
+const iframePermissions = [
+  'accelerometer',
+  'ambient-light-sensor',
+  'autoplay',
+  'battery',
+  'camera',
+  'fullscreen',
+  'gamepad',
+  'geolocation',
+  'gyroscope',
+  'idle-detection',
+  'magnetometer',
+  'microphone',
+  'midi',
+  'payment',
+  'picture-in-picture',
+  'screen-wake-lock',
+  'usb',
+].join('; ');
