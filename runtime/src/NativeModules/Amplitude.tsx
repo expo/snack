e92c8aeee5324940c9aa0879bceb1c4d01a6d430
Cedric van Putten <me@bycedric.com>
2023-10-11 14:57:02 +0200

    refactor(runtime): clean up unused amplitude and analytics code (#492)

--- runtime/src/NativeModules/Amplitude.tsx
+++
@@ -1,9 +0,0 @@
-export default {
-  initializeAsync(_key: string) {
-    return Promise.resolve(true);
-  },
-
-  logEventWithPropertiesAsync(_type: string, _payload: object) {
-    return Promise.resolve(true);
-  },
-};
