88ecb018c86bad2d3c1bb45ae3f005c8cc2a2202
Mike Hampton <randomhajile@users.noreply.github.com>
2024-01-08 09:09:01 -0500

    @mike/secrets (#521)
    * refactor(snackager): use ExternalSecret
    
    * refactor(snackpub): use ExternalSecret
    
    * refactor(website): remove secret
    
    * refactor(ci): remove git-crypt usage

--- snackager/.env-cmdrc.js
+++ snackager/.env-cmdrc.js
@@ -1,27 +1,43 @@
 const fs = require('fs');
 const path = require('path');
+const yaml = require('js-yaml');
 const { GetEnvVars } = require('env-cmd');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager').v1;
+
+async function getSecretEnv(name) {
+  const secretmanagerClient = new SecretManagerServiceClient();
+  try {
+    const response = await secretmanagerClient.accessSecretVersion({ name });
+    return JSON.parse(response[0].payload.data.toString());
+  } catch {
+    return {};
+  }
+}
 
 module.exports = (async function () {
   const processArgs = process.argv.join(' ');
   const baseEnv = await GetEnvVars({ envFile: { filePath: './k8s/base/snackager.env' } });
+  const stagingEnv = await GetEnvVars({ envFile: { filePath: './k8s/staging/snackager.env' } });
   const baseSecrets = {
-    SENTRY_DSN: fs.readFileSync('./k8s/base/secrets/SENTRY_DSN').toString(),
     REDIS_URL: 'redis://localhost:6379/0', // proxied by port-forward-redis
   };
-  const stagingEnv = await GetEnvVars({ envFile: { filePath: './k8s/staging/snackager.env' } });
-  const stagingSecrets = await GetEnvVars({
-    envFile: { filePath: './k8s/staging/secrets/snackager.env' },
-  });
-
+  const externalSecret = yaml.load(fs.readFileSync(
+    './k8s/staging/external-secret-env.yaml',
+    'utf8',
+  ));
+  const secretName = externalSecret.spec.dataFrom[0].extract.key;
+  const secretVersion = externalSecret.spec.dataFrom[0].extract.version;
+  const secretResourceName = `projects/77257980902/secrets/${secretName}/versions/${secretVersion}`;
+  const stagingSecrets = await getSecretEnv(secretResourceName);
   delete stagingSecrets['REDIS_URL']; // this is set above for the proxy
 
   if (!stagingSecrets.GIT_SESSION_SECRET && !processArgs.includes('env-cmd -e test')) {
     console.error(
-      'Secrets are locked, unable to start Snackager. External contributors cannot start Snackager and can ignore this error. snack-proxies will redirect traffic to the Snackager service running on the staging environment.\n'
+      'Cannot access secrets, unable to start Snackager. External contributors cannot start Snackager and can ignore this error. snack-proxies will redirect traffic to the Snackager service running on the staging environment.\n'
     );
     throw new Error('Secrets are locked.');
   }
+
   return {
     development: {
       NODE_ENV: 'development',
