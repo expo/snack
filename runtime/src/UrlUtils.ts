791774a723d36da42686db4ff15be425f4ef9b78
Test User <github@cedric.dev>
2024-01-12 19:13:59 +0100

    refactor(runtime): update URL parsing for EAS Update URLs

--- runtime/src/UrlUtils.ts
+++ runtime/src/UrlUtils.ts
@@ -1,5 +1,30 @@
-export function parseExperienceURL(
-  experienceURL: string
+import { parse as parseQueryString } from 'query-string';
+
+/**
+ * Parse the query parameters of the URL to extract the channel, and optional test transport.
+ * Note, this is backported and should not be used in new code.
+ */
+export function parseExperienceURL(url: string) {
+  try {
+    const qs = parseQueryString(url.split('?')[1] || '');
+    if (qs['snack-channel']) {
+      return {
+        channel: qs['snack-channel'] as string,
+        testTransport: qs['testTransport'] as string | null,
+      };
+    }
+  } catch {
+    // Pass through
+  }
+
+  return null;
+}
+
+/**
+ * @deprecated This format of URLs is being phased out
+ */
+export function parseClassicExperienceURL(
+  experienceURL: string,
 ): { channel: string; testTransport: string | null } | null {
   const matches = experienceURL.match(/(\+|\/sdk\..*-)([^?]*)\??(.*$)/);
   if (!matches) {
