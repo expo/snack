8b93f9c1d3b61f6bb36e5520160e8f68dcfbd9aa
Cedric van Putten <github@cedric.dev>
2024-01-19 19:08:08 +0100

    refactor: change default test sdk to 50

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -4,4 +4,4 @@ export const defaultSdkVersion: SDKVersion = '49.0.0';
 
 // Mostly used for tests
 export const oldestSdkVersion: SDKVersion = '47.0.0';
-export const newestSdkVersion: SDKVersion = '49.0.0';
+export const newestSdkVersion: SDKVersion = '50.0.0';
