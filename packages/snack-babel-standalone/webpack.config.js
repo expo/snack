d775ed371273f090316d07f9a02cdc816d985a4f
Cedric van Putten <github@cedric.dev>
2024-01-22 15:08:30 +0100

    refactor(babel-standalone): upgrade babel packages and swap to new babel preset

--- packages/snack-babel-standalone/webpack.config.js
+++ packages/snack-babel-standalone/webpack.config.js
@@ -39,7 +39,7 @@ const config = {
           loader: 'babel-loader',
           options: {
             presets: [
-              require('metro-react-native-babel-preset').getPreset(null, {
+              require('@react-native/babel-preset').getPreset(null, {
                 enableBabelRuntime: false,
               }),
             ],
