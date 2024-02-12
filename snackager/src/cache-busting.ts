eb77f7876030c5a5724f3237ffeea4ffd501b541
Cedric van Putten <github@cedric.dev>
2024-02-12 17:09:39 +0100

    chore(snackager): update cache busing for final staging test

--- snackager/src/cache-busting.ts
+++ snackager/src/cache-busting.ts
@@ -11,13 +11,14 @@ const cacheBusting: { version: number; packages: { [name: string]: number } } = 
    * requested.
    * */
   packages: {
-    'react-native-reanimated': 2,
-    moti: 2,
-    '@motify/interactions': 2,
-    '@motify/skeleton': 2,
-    '@draftbit/ui': 2,
-    '@shopify/react-native-skia': 1,
+    'react-native-reanimated': 3,
+    moti: 3,
+    '@motify/interactions': 3,
+    '@motify/skeleton': 3,
+    '@draftbit/ui': 1,
+    '@shopify/react-native-skia': 3,
     '@expo-google-fonts/.*': 2,
+    '@react-navigation/drawer': 3,
   },
 };
 
