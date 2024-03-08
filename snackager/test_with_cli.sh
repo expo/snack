ae7e7b00df84657d0c54bf08b6ab62a9f94c99ba
Mike Hampton <randomhajile@users.noreply.github.com>
2024-03-08 09:53:55 -0500

    chore: migrate to Artifact Registry (#571)

--- snackager/test_with_cli.sh
+++ snackager/test_with_cli.sh
@@ -9,7 +9,7 @@ if [[ -z "$tag" ]]
 then
   tag="$(env LC_CTYPE=C tr -dc 'a-zA-Z0-9' </dev/urandom | fold -w 32 | head -n 1)"
   echo "no tag specified, building docker image with tag $tag"
-  gcloud docker -- build -t "gcr.io/exponentjs/snackager:$tag" .
+  gcloud docker -- build -t "us-central1-docker.pkg.dev/exponentjs/snack/snackager:$tag" .
 fi
 
-docker run -t -w="/app" "gcr.io/exponentjs/snackager:$tag" yarn bundle "$package"
+docker run -t -w="/app" "us-central1-docker.pkg.dev/exponentjs/snack/snackager:$tag" yarn bundle "$package"
