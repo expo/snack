6aa609e592cf46f641efef5f1e1dc1518f3fb6d2
Cedric van Putten <github@cedric.dev>
2024-04-25 17:28:52 +0200

    refactor(website): configure new Expo SDK

--- website/src/client/configs/sdk.tsx
+++ website/src/client/configs/sdk.tsx
@@ -10,7 +10,8 @@ export const versions: Record<SDKVersion, boolean> = {
   '48.0.0': true,
   '49.0.0': true,
   '50.0.0': true,
+  '51.0.0': false,
 };
 
 export const DEFAULT_SDK_VERSION: SDKVersion = defaultSdkVersion;
-export const TEST_SDK_VERSION: SDKVersion = '49.0.0';
+export const TEST_SDK_VERSION: SDKVersion = '50.0.0';
