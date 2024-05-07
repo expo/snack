79959fb9432b9c6517c8a186e7dd6f4350df533d
Cedric van Putten <me@bycedric.com>
2024-05-07 20:09:51 +0200

    refactor(snack-content): set SDK 51 as default (#579)
    * refactor(snack-content): set SDK 51 as default
    
    * test(snack-sdk): update snapshot test
    
    * test(snackager): update snapshot test

--- packages/snack-content/src/defaults.ts
+++ packages/snack-content/src/defaults.ts
@@ -1,7 +1,7 @@
 import { SDKVersion } from './types';
 
-export const defaultSdkVersion: SDKVersion = '50.0.0';
+export const defaultSdkVersion: SDKVersion = '51.0.0';
 
 // Mostly used for tests
-export const oldestSdkVersion: SDKVersion = '48.0.0';
-export const newestSdkVersion: SDKVersion = '50.0.0';
+export const oldestSdkVersion: SDKVersion = '49.0.0';
+export const newestSdkVersion: SDKVersion = '51.0.0';
