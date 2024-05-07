c3548a0dc844ecc7f972b24297e1b093410d8398
Cedric van Putten <github@cedric.dev>
2024-05-07 23:01:36 +0200

    test(runtime): update outdated snack urls test

--- runtime/src/__tests__/UrlUtils-test.ts
+++ runtime/src/__tests__/UrlUtils-test.ts
@@ -1,14 +1,40 @@
-import { parseExperienceURL } from '../UrlUtils';
+import { parseExperienceURL, parseClassicExperienceURL } from '../UrlUtils';
 
 describe(parseExperienceURL, () => {
   it('should parse snack url', () => {
-    const result = parseExperienceURL('exp://exp.host/@snack/sdk.47.0.0-4AQkc5pxqe');
+    const result = parseExperienceURL('exp://u.expo.dev/xxx/?snack-channel=4AQkc5pxqe');
+    expect(result?.channel).toBe('4AQkc5pxqe');
+    expect(result?.testTransport).toBeUndefined();
+  });
+
+  it('should parse snack url with testTransport', () => {
+    const result = parseExperienceURL(
+      'exp://u.expo.dev/xxx/?snack-channel=4AQkc5pxqe&foo=foo&testTransport=snackpub&bar=bar'
+    );
+    expect(result?.channel).toBe('4AQkc5pxqe');
+    expect(result?.testTransport).toBe('snackpub');
+  });
+
+  it('should return null for account snack full name url without channel', () => {
+    const result = parseExperienceURL('exp://u.expo.dev/xxx');
+    expect(result).toBe(null);
+  });
+
+  it('should return null for invalid url', () => {
+    const result = parseExperienceURL('exp://exp.host/');
+    expect(result).toBe(null);
+  });
+});
+
+describe(parseClassicExperienceURL, () => {
+  it('should parse snack url', () => {
+    const result = parseClassicExperienceURL('exp://exp.host/@snack/sdk.47.0.0-4AQkc5pxqe');
     expect(result?.channel).toBe('4AQkc5pxqe');
     expect(result?.testTransport).toBe(null);
   });
 
   it('should parse snack url with testTransport', () => {
-    const result = parseExperienceURL(
+    const result = parseClassicExperienceURL(
       'exp://exp.host/@snack/sdk.47.0.0-4AQkc5pxqe?foo=foo&testTransport=snackpub&bar=bar'
     );
     expect(result?.channel).toBe('4AQkc5pxqe');
@@ -16,13 +42,13 @@ describe(parseExperienceURL, () => {
   });
 
   it('should parse account snack full name url', () => {
-    const result = parseExperienceURL('exp://exp.host/@johndoe/the-snack+4AQkc5pxqe');
+    const result = parseClassicExperienceURL('exp://exp.host/@johndoe/the-snack+4AQkc5pxqe');
     expect(result?.channel).toBe('4AQkc5pxqe');
     expect(result?.testTransport).toBe(null);
   });
 
   it('should parse account snack full name url with testTransport', () => {
-    const result = parseExperienceURL(
+    const result = parseClassicExperienceURL(
       'exp://exp.host/@johndoe/the-snack+4AQkc5pxqe?foo=foo&testTransport=snackpub&bar=bar'
     );
     expect(result?.channel).toBe('4AQkc5pxqe');
@@ -30,12 +56,12 @@ describe(parseExperienceURL, () => {
   });
 
   it('should return null for account snack full name url without channel', () => {
-    const result = parseExperienceURL('exp://exp.host/@johndoe/the-snack');
+    const result = parseClassicExperienceURL('exp://exp.host/@johndoe/the-snack');
     expect(result).toBe(null);
   });
 
   it('should return null for invalid url', () => {
-    const result = parseExperienceURL('exp://exp.host/');
+    const result = parseClassicExperienceURL('exp://exp.host/');
     expect(result).toBe(null);
   });
 });
