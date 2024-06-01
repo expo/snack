9238788349d2ae338cf07120b1d7b76a801a9435
Cedric van Putten <github@cedric.dev>
2024-06-01 13:12:26 +0200

    refactor(runtime): backport the constants file from SDK 49

--- runtime/app.config.js
+++ runtime/app.config.js
@@ -1,9 +1,39 @@
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      ...config.extra,
-      cloudEnv: process.env.CLOUD_ENV,
-    },
-  };
+const assert = require('assert');
+const { string } = require('getenv');
+
+/**
+ * @param {object} params
+ * @param {import('expo/config').ExpoConfig} params.config
+ * @returns {import('expo/config').ExpoConfig}
+ */
+module.exports = ({ config }) => {
+  // Set correct cloudenv for the app
+  config.extra ||= {};
+  config.extra.cloudEnv = process.env.CLOUD_ENV;
+
+  const projectId = string('EXPO_PROJECT_ID', null);
+
+  // Dynamically configure the EAS project ID, if not configured yet.
+  if (!config.extra?.eas?.projectId) {
+    assert(projectId, 'Environment variable "EXPO_PROJECT_ID" is required');
+
+    config.extra ||= {};
+    config.extra.eas ||= {};
+    config.extra.eas.projectId = projectId;
+  }
+
+  // Dynamically configure the EAS endpoint, if not configured yet.
+  if (!config.updates?.url) {
+    assert(projectId, 'Environment variable "EXPO_PROJECT_ID" is required');
+
+    config.updates ||= {};
+
+    if (string('EXPO_PUBLIC_SNACK_ENV') === 'staging') {
+      config.updates.url = `https://staging-u.expo.dev/${projectId}`;
+    } else {
+      config.updates.url = `https://u.expo.dev/${projectId}`;
+    }
+  }
+
+  return config;
 };
--- runtime/src/Modules.tsx
+++ runtime/src/Modules.tsx
@@ -24,6 +24,7 @@ import * as context from 'snack-require-context';
 import { SourceMapConsumer, RawSourceMap } from 'source-map';
 
 import System from '../vendor/system.src';
+import { SNACKAGER_API_URLS } from './Constants';
 import * as Files from './Files';
 import * as Logger from './Logger';
 import AssetRegistry from './NativeModules/AssetRegistry';
@@ -43,9 +44,6 @@ type Load = {
     sourceMap?: RawSourceMap;
   };
 };
-
-const SNACKAGER_CDN_STAGING = 'https://ductmb1crhe2d.cloudfront.net';
-const SNACKAGER_CDN_PROD = 'https://d37p21p3n8r8ug.cloudfront.net';
 
 // This is super hacky
 // This avoids a bug where for some reason `react` is `undefined` in a dependency
@@ -312,12 +310,9 @@ const fetchPipeline = async (load: Load) => {
             `from cache ${bundle ? bundle.length : undefined} bytes`
           );
         } else {
-          // In development, try fetching from staging cloudfront first
-          const cloudFrontUrls =
-            Constants.manifest?.extra?.cloudEnv !== 'production'
-              ? [SNACKAGER_CDN_STAGING, SNACKAGER_CDN_PROD]
-              : [SNACKAGER_CDN_PROD];
-          for (const url of cloudFrontUrls) {
+          for (const [i, url] of SNACKAGER_API_URLS.entries()) {
+            // Determine if there is another URL to try on fetch failures
+            const hasNextUrl = i < SNACKAGER_API_URLS.length - 1;
             const fetchFrom = `${url}/${handle}-${Platform.OS}/bundle.js`;
 
             try {
@@ -331,7 +326,7 @@ const fetchPipeline = async (load: Load) => {
                 throw new Error(`Request failed with status ${res.status}: ${res.statusText}`);
               }
             } catch (e) {
-              if (url !== SNACKAGER_CDN_STAGING) {
+              if (!hasNextUrl) {
                 Logger.error('Error fetching bundle', fetchFrom, e);
                 throw e;
               } else {
