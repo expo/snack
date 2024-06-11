b7ecaf6910805d560270c3be81c292bfc2aa96f7
Cedric van Putten <github@cedric.dev>
2024-06-11 16:34:30 +0200

    fix(runtime): use `experiments.baseUrl` for web exports

--- runtime/metro.config.js
+++ runtime/metro.config.js
@@ -6,10 +6,8 @@ const config = getDefaultConfig(__dirname);
 
 // Workaround for paths hosting web on a subdirectory (https://<s3-bucket>/v2/<expo-sdk-version>/)
 if (boolish('SNACK_EXPORT_WEB', false)) {
-  const expoVersion = require('expo/package.json').version;
-  const semver = require('semver');
-
-  config.transformer.publicPath = `/v2/${semver.major(expoVersion)}/assets`;
+  // This is now handled by the **app.json** `expo.experiments.baseUrl` property instead
+  // config.transformer.publicPath = `/v2/${semver.major(expoVersion)}/assets`;
 
   // Error: While trying to resolve module `@socket.io/component-emitter` from file `/runtime/node_modules/socket.io-parser/build/esm/index.js`,
   // the package `/runtime/node_modules/@socket.io/component-emitter/package.json` was successfully found.
