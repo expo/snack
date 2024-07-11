c11cb171049bfd28df01bb40775a81658105e135
Cedric van Putten <github@cedric.dev>
2024-07-11 14:30:31 +0200

    refactor(snackager): realign supported file extensions for Snackager with `@expo/metro-config`

--- snackager/src/bundler/getResolverConfig.ts
+++ snackager/src/bundler/getResolverConfig.ts
@@ -7,16 +7,26 @@ export default function getResolverConfig(platform: string): ResolveOptions {
     // TODO: figure out whether AssetResolver as a plugin is obsolete and can be removed
     plugins: [new AssetResolver({ platform })],
     mainFields: [...(platform !== 'web' ? ['react-native'] : []), 'browser', 'module', 'main'],
-    extensions: [
-      `.${platform}.tsx`,
-      `.${platform}.ts`,
-      `.${platform}.cjs`,
-      `.${platform}.js`,
-      ...(platform !== 'web' ? ['.native.tsx', '.native.ts', '.native.js', '.native.cjs'] : []),
-      '.tsx',
-      '.ts',
-      '.cjs',
-      '.js',
-    ],
+    extensions: createPlatformExtensions(platform),
   };
+}
+
+// See @expo/metro-config extensions (EXPO_DEBUG=true npx expo start)
+const EXTENSIONS_LANGUAGE = ['ts', 'tsx', 'mjs', 'js', 'jsx', 'json', 'cjs'];
+const EXTENSIONS_PLATFORM = {
+  web: ['web', ''],
+  ios: ['ios', 'native', ''],
+  android: ['android', 'native', ''],
+};
+
+function createPlatformExtensions(platform: string) {
+  const extensions: string[] = [];
+
+  for (const platformExt of EXTENSIONS_PLATFORM[platform] ?? ['']) {
+    for (const langExt of EXTENSIONS_LANGUAGE) {
+      extensions.push(platformExt ? `.${platformExt}.${langExt}` : `.${langExt}`);
+    }
+  }
+
+  return extensions;
 }
