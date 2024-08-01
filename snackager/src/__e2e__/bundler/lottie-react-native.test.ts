6f036f833faecb4a7c769144e8ac0b1c6cf5b4ec
Cedric van Putten <github@cedric.dev>
2024-08-01 13:29:49 +0200

    test(snackager): add `lottie-react-native@6.7.0` test

---
+++ snackager/src/__e2e__/bundler/lottie-react-native.test.ts
@@ -0,0 +1,14 @@
+import { bundleAsync, normalizeBundleSize } from '../bundleAsync';
+
+it('creates bundle for lottie-react-native@6.7.0', async () => {
+  const bundle = await bundleAsync('lottie-react-native@6.7.0');
+  expect(normalizeBundleSize(bundle)).toMatchSnapshot();
+  // react-native/Libraries/NativeComponent/ViewConfigIgnore should be included as an external
+  expect(bundle.files.ios['bundle.js'].externals).toEqual(
+    expect.arrayContaining(['react-native/Libraries/NativeComponent/ViewConfigIgnore']),
+  );
+  // react-native/Libraries/ReactNative/RendererProxy should be included as an external
+  expect(bundle.files.ios['bundle.js'].externals).toEqual(
+    expect.arrayContaining(['eact-native/Libraries/ReactNative/RendererProxy']),
+  );
+});
