453c6916591f5ff596c12277e3cc98fcd3d4d463
Cedric van Putten <github@cedric.dev>
2025-01-03 12:11:47 +0100

    test(snackager): add e2e bundling test for `react-native-webview@13.12.5`

--- snackager/src/__e2e__/bundler/react-native-webview.test.ts
+++ snackager/src/__e2e__/bundler/react-native-webview.test.ts
@@ -1,9 +1,17 @@
 import { bundleAsync, normalizeBundleSize } from '../bundleAsync';
 
-it('creates bundle for react-native-webview', async () => {
+it('creates bundle for react-native-webview@13.7.0', async () => {
   const bundle = await bundleAsync('react-native-webview@13.7.0');
   expect(normalizeBundleSize(bundle)).toMatchSnapshot();
-  // react-native-webview contains a direct reference to
+  // react-native-webview@13.7.0 contains a direct reference to
   // 'react-native/Libraries/BatchedBridge/BatchedBridge` and
   // should bundle correctly
+});
+
+it('creates bundle for react-native-webview@13.12.5', async () => {
+  const bundle = await bundleAsync('react-native-webview@13.12.5');
+  expect(normalizeBundleSize(bundle)).toMatchSnapshot();
+  // react-native-webview@13.12.5 imports the vendored event emitter
+  // from react-native, which is using the `[key in keyof TEventToArgsMap]`
+  // syntax and requires newer flow parsers.
 });
