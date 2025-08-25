ed5c27e38342f563bb316d3f078053975e1532f0
Cedric van Putten <github@cedric.dev>
2025-08-25 13:30:24 +0200

    fix(website): drop `SafeAreaView` from default template

--- website/src/client/configs/defaults.tsx
+++ website/src/client/configs/defaults.tsx
@@ -8,7 +8,7 @@ export const DEFAULT_DESCRIPTION = 'No description';
 
 export const DEFAULT_CODE: SnackFiles = {
   'App.js': {
-    contents: `import { Text, SafeAreaView, StyleSheet } from 'react-native';
+    contents: `import { Text, StyleSheet } from 'react-native';
 
 // You can import supported modules from npm
 import { Card } from 'react-native-paper';
@@ -18,14 +18,14 @@ import AssetExample from './components/AssetExample';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.paragraph}>
         Change code in the editor and watch it change on your phone! Save to get a shareable url.
       </Text>
       <Card>
         <AssetExample />
       </Card>
-    </SafeAreaView>
+    </View>
   );
 }
 
